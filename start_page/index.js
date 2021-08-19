3d17340f29808fa047e63c7dee41e678cd6e30d9
kevinsanders <fairytailetyp@gmail.com>
2021-08-19 21:39:15 +0200

    commit

--- start_page/index.js
+++ start_page/index.js
@@ -1,13 +1,13 @@
-const express = require("express");
-const path = require("path");
+const express = require('express');
+const path = require('path');
 const app = express();
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(`${__dirname}/public`));
 
 app.get('/', (req, res) => {
-    res.sendFile(path.resolve(__dirname, ".", "public", "index.html"));
+    res.sendFile(path.resolve(__dirname, '.', 'public', 'index.html'));
 })
 
 app.listen(3000, () => {
-  console.log("Listening");
+  console.log('Listening');
 });
